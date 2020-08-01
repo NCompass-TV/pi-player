aaab7450f75fe3d369b4ec5bdafeae01b881b8c9
earlvhin <earlgabuat@gmail.com>
2020-08-02 06:45:46 +0800

    to prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,23 +6,23 @@ export const environment = {
   registerLicense: 'license/register',
   
   // Nodejs Server
-  public_url: 'http://localhost:3215/api',
-  systemInfo: '/systeminfo',
-  saveLicense: '/save-settings/',
-  saveData: '/save-data',
-  selectTemplate: '/template',
-  saveDataAndDownload: '/content',
-  clearDatabase: '/content/cleardb',
-  hasLicense: '/content/has-license',
-  saveContentCount: '/save-content-count',
-  saveLicensetoDb: '/license/save-license',
-  getLicenseFromDb: '/license/get-license',
-  resetPlayer: '/content/reset',
-  screenshot: '/utils/screenshot',
-  playlist: '/playlist/',
+  public_url: 'http://localhost:3215',
+  systemInfo: '/api/systeminfo',
+  saveLicense: '/api/save-settings/',
+  saveData: '/api/save-data',
+  selectTemplate: '/api/template',
+  saveDataAndDownload: '/api/content',
+  clearDatabase: '/api/content/cleardb',
+  hasLicense: '/api/content/has-license',
+  saveContentCount: '/api/save-content-count',
+  saveLicensetoDb: '/api/license/save-license',
+  getLicenseFromDb: '/api/license/get-license',
+  resetPlayer: '/api/content/reset',
+  screenshot: '/api/utils/screenshot',
+  playlist: '/api/playlist/',
   kafka_send_to_broker: '/send-content-count-data',
 
   // Misc
   pi_socket: 'http://localhost:3215',
-  kafka_topic: 'content-count',
+  kafka_topic: 'content-count'
 };
