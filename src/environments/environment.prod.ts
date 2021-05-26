2a6a7fe3e960ff61e9508da54bbdf55ecdde80ec
earlvhin <earlgabuat@gmail.com>
2021-05-26 13:21:31 +0800

    Staging API

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,30 +1,30 @@
 export const environment = {
-  production: true,
-  // DotNet Server
-  server_url: 'https://nctvapi.n-compass.online/api/',
-  piDownloader: 'pidownloader/getcontents?licensekey=',
-  registerLicense: 'license/register',
-  
-  // Nodejs Server
-  public_url: 'http://localhost:3215',
-  systemInfo: '/api/systeminfo',
-  saveLicense: '/api/save-settings/',
-  saveData: '/api/save-data',
-  selectTemplate: '/api/template',
-  saveDataAndDownload: '/api/content',
-  clearDatabase: '/api/content/cleardb',
-  hasLicense: '/api/content/has-license',
-  saveContentCount: '/api/save-content-count',
-  saveLicensetoDb: '/api/license/save-license',
-  getLicenseFromDb: '/api/license/get-license',
-  resetPlayer: '/api/content/reset',
-  refetchPlayer: '/api/content/refetch',
-  screenshot: '/api/utils/screenshot',
-  playlist: '/api/playlist/',
-  kafka_send_to_broker: '/send-content-count-data',
+	production: true,
+	// DotNet Server
+	server_url: 'https://stgapi.n-compass.online/api/',
+	piDownloader: 'pidownloader/getcontents?licensekey=',
+	registerLicense: 'license/register',
 
-  // Misc
-  pi_socket: 'http://localhost:3215',
-  socket_server: 'https://nctvsocket.n-compass.online',
-  kafka_topic: 'content-count'
+	// Nodejs Server
+	public_url: 'http://localhost:3215',
+	systemInfo: '/api/systeminfo',
+	saveLicense: '/api/save-settings/',
+	saveData: '/api/save-data',
+	selectTemplate: '/api/template',
+	saveDataAndDownload: '/api/content',
+	clearDatabase: '/api/content/cleardb',
+	hasLicense: '/api/content/has-license',
+	saveContentCount: '/api/save-content-count',
+	saveLicensetoDb: '/api/license/save-license',
+	getLicenseFromDb: '/api/license/get-license',
+	resetPlayer: '/api/content/reset',
+	refetchPlayer: '/api/content/refetch',
+	screenshot: '/api/utils/screenshot',
+	playlist: '/api/playlist/',
+	kafka_send_to_broker: '/send-content-count-data',
+
+	// Misc
+	pi_socket: 'http://localhost:3215',
+	socket_server: 'https://stgsocket.n-compass.online',
+	kafka_topic: 'content-count'
 };
