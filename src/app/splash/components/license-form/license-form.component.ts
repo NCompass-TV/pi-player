d8edae12ef288f1c100c9246c456d2d03ebe891e
earlvhin <earlgabuat@gmail.com>
2021-05-27 04:38:45 -0500

    Add License Checker on Setup

--- src/app/splash/components/license-form/license-form.component.ts
+++ src/app/splash/components/license-form/license-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
@@ -14,6 +14,7 @@ import { License } from '../../../models/license.model';
 
 export class LicenseFormComponent implements OnInit {
 
+	@Input() license_key: string;
 	internal_server: boolean = false;
 	is_disabled: boolean = true;
 	is_submitted: boolean = false;
@@ -37,6 +38,8 @@ export class LicenseFormComponent implements OnInit {
 				license: ['', Validators.required]
 			}
 		)
+
+		this.f.license.setValue(this.license_key)
 
 		this.subscription.add(
 			this.register_license_form.valueChanges.subscribe(
--- src/app/splash/pages/register-license/register-license.component.ts
+++ src/app/splash/pages/register-license/register-license.component.ts
@@ -7,6 +7,8 @@ import { environment } from '../../../../environments/environment';
 
 import { Observable, fromEvent, merge, of } from 'rxjs';
 import { mapTo } from 'rxjs/operators';
+import { License } from 'src/app/models/license.model';
+import { PiInfo } from 'src/app/models/pi-info.model';
 
 @Component({
 	selector: 'app-register-license',
@@ -19,6 +21,9 @@ export class RegisterLicenseComponent implements OnInit {
 	online$: Observable<boolean>;
 	subscription: Subscription = new Subscription;
 	is_connected: boolean;
+	license_key: string;
+	checking_license: boolean = true;
+	license_exist: boolean = false;
 
 	constructor(
 		private _player_service: PlayerService,
@@ -59,16 +64,68 @@ export class RegisterLicenseComponent implements OnInit {
 				(data: any[]) => {
 					if(data.length != 0) {
 						if (this.is_connected) {
-							console.log('License detected and player is online');
-							this._router.navigate(['/setup/getting-ready']);
+							console.log('License detected and player is online', data);
+							this.license_key = data[0].license_key;
+							this.license_exist = true;
+							this.getDeviceInfo();
 						} else {
 							console.log('License detected but internet has failed, Redirecting to player');
 							this._router.navigate(['/player']);
 						}
 					} else {
 						console.log('No License Detected. Is Player Online:',  this.is_connected);
+						this.checking_license = false;
+						this.license_exist = false;
 						return false;
 					}
+				}
+			)
+		)
+	}
+
+	// 2. Pi: Get Pi Player System Info -> This is required and included on registerLicense(license_key)
+	getDeviceInfo() {
+		this.subscription.add(
+			this._player_service.get_pi_info().subscribe(
+				data => {
+					this.registerLicense(this.mapPiSystemnInfo(data));
+				},
+				error => {
+					console.log(error);
+				}
+			)
+		)
+	}
+
+	// 3. Pi: Map Pi System Info to format data
+	mapPiSystemnInfo(pi) {
+		return new PiInfo (
+			this.license_key,
+			pi.macAddress ? pi.macAddress : 'N/A',
+			pi.memory,
+			pi.internetType,
+			pi.internetSpeed,
+			pi.storage.total,
+			`${100 - pi.storage.used} %`,
+			JSON.stringify(pi.appVersion)
+		)
+	}
+
+	// 4. Dashboard Server: Check if License Exist in Server then save
+	registerLicense(license_info) {
+		this.subscription.add(
+			this._player_service.register_license(license_info).subscribe(
+				(data: License) => {
+					console.log(data);
+					if(data.message) {
+						console.log('#LicenseFormComponent - registerLicense() - Error:', data.message);
+					} else {
+						this._router.navigate(['/setup/getting-ready']);
+					}
+				}, 
+				error => {
+					// Must add redirection to Fatal Error Page...
+					console.log('#LicenseFormComponent - registerLicense() - Fatal Error:', error);
 				}
 			)
 		)
